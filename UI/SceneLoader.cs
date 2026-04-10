fad4463a38c0718a560e1cd626189768a0bf125b
wojna2000 <wojtek.marzec2002@gmail.com>
2026-04-10 20:05:56 +0200

    Main Menu

---
+++ UI/SceneLoader.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Wczytuje scenê po nazwie (string)
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Zamyka grê
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        // To tylko ¿eby dzia³a³o w edytorze Unity
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
